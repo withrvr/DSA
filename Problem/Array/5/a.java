6dc2dc8e0d058c008d8f9da49eb0683ee37faccb
Raghav Rathi <ytwithrvr@gmail.com>
2022-12-22 00:11:37 +0530

    👍: (5-array) : a.java

---
+++ Problem/Array/5/a.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class a {
+	public static void main(String[] args) {
+		int[] arr = { -12, -11, 13, 5, 6, -7, 5, -3, -6 };
+		// int[] arr = { -12, 11, 13, 5, 6, -7, 5, -3, -6 };
+
+		// pos ... first pos no to replace with
+		for (int i = 0, pos = 0, temp; i < arr.length; i++) {
+			if (arr[i] < 0) {
+				if (i != pos) { // swap with itself ... no use ... // basically for series of starting -ve nos
+								// it work
+					temp = arr[i];
+					arr[i] = arr[pos];
+					arr[pos] = temp;
+				}
+				pos++;
+			}
+		}
+
+		System.out.println(Arrays.toString(arr));
+	}
+}
---
+++ Problem/Array/5/readme.md
@@ -0,0 +1,13 @@
+# 📄: (4-Array) -> Move all the negative elements to one side of the array
+
+- [a.java](./a.java)
+  - track which is the first pos of +ve
+  - no then replace it with -ve no which is next
+<!--
+- [b.java](./b.java)
+  - learn
+  - learn
+ -->
+# 🔗: Links
+
+- [GFG Page](https://www.geeksforgeeks.org/move-negative-numbers-beginning-positive-end-constant-extra-space/)
